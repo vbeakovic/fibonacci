26355e9f1eebc72c9b932bebeb36a723876d1a35
vbeakovic <valter.beakovic@gmail.com>
2017-08-06 10:13:35 +0200

    fibonacci router

---
+++ routes/fibonacci.js
@@ -0,0 +1,22 @@
+const express = require('express');
+var router = express.Router();
+
+var math = require(../math);
+
+router.get('/', function(req, res,next) {
+  if (req.query.fibonum) {
+    // Calculate directly in this server
+    res.render('fibonacci', {
+      title: "Calculate Fibonacci numbers",
+      fibonum: req.query.fibonum,
+      fiboval: math.fibonacci(req.query.fibonum)
+    });
+  } else {
+    res.render('fibonacci', {
+      title: 'Calculate Fibonacci numbers',
+      fiboval: undefinded
+    });
+  }
+});
+
+module.exports = router;
