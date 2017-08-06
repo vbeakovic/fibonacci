ff1602b5fec3478b39f069b289ff00765ef1696d
vbeakovic <valter.beakovic@gmail.com>
2017-08-06 17:46:56 +0200

    First commit

---
+++ wget.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const url = require('url');
+const util = require('util');
+
+var argUrl = process.argv[2];
+var parsedUrl = url.parse(argUrl, true);
+
+var options = {
+  host: parsedUrl.hostname,
+  port: parsedUrl.port,
+  path: parsedUrl.pathname,
+  method: 'GET'
+};
+
+if (parsedUrl.search) options.path += "?"+parsedUrl.search;
+
+var req = http.request(options);
+
+req.on('response', res => {
+  util.log('STATUS: ' + res.statusCode);
+  util.log('HEADERS: ' + util.inspect(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', chunk => {util.log('BODY: ' + chunk); });
+  res.on('error', err => {util.log('RESPONSE ERROR: ' + err); });
+});
+
+req.on('error', err => {util.log('REQUEST ERROR: ' + err); });
+req.end();
