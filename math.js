d9d1caa2900ae8b6047a5c7847a6ba7bb65f6da7
vbeakovic <valter.beakovic@gmail.com>
2017-08-06 15:43:28 +0200

    typo corrected

--- math.js
+++ math.js
@@ -1,19 +1,19 @@
 var fibonacci = exports.fibonacci = function(n, done) {
-  if (n === 0)
-    done(undefined, 0);
-  else if (n === 1 || n === 2)
-      done(undefined, 1);
-  else {
-    setImmediate(function() {
-      fibonacci(n-1, function(err, val1) {
-        if (err) done(err);
-        else setImmediate(function() {
-          fibonacci(n-2, function(err, val2) {
-            if (err) done(err);
-            else done(undefined, val1+val2);
-          });
+    if (n === 0)
+        done(undefined, 0);
+    else if (n === 1 || n === 2)
+        done(undefined, 1);
+    else {
+        setImmediate(() => {
+            fibonacci(n-1, (err, val1) => {
+                if (err) done(err);
+                else setImmediate(() => {
+                    fibonacci(n-2, (err, val2) => {
+                        if (err) done(err);
+                        else done(undefined, val1+val2);
+                    });
+                });
+            });
         });
-      });
-    });
-  }
-}
+    }
+};
