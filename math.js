41f059ef6ab25c163a703cdc1b3977872236c4b2
vbeakovic <valter.beakovic@gmail.com>
2017-08-06 12:48:51 +0200

    fibonacci algorith refactored

--- math.js
+++ math.js
@@ -1,5 +1,10 @@
 var fibonacci = exports.fibonacci = function(n) {
-  if (n === 1) return 1;
-  else if (n === 2) return 1;
-  else return fibonacci(n-1) + fibonacci(n-2);
+  var fibos = [];
+  fibos[0] = 0;
+  fibos[1] = 1;
+  fibos[2] = 2;
+  for (var i = 3; i <= n; i++) {
+    fibos[i] = fibos[i-2] + fibos[i-1];
+  }
+  return fibos[n];
 }
