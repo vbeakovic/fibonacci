922f1347204d01b41d49fe76c4a423e953e33d17
vbeakovic <valter.beakovic@gmail.com>
2017-08-06 12:55:56 +0200

    third version offibonacci algoritd

--- math.js
+++ math.js
@@ -1,10 +1,19 @@
-var fibonacci = exports.fibonacci = function(n) {
-  var fibos = [];
-  fibos[0] = 0;
-  fibos[1] = 1;
-  fibos[2] = 2;
-  for (var i = 3; i <= n; i++) {
-    fibos[i] = fibos[i-2] + fibos[i-1];
+var fibonacci = exports.fibonacci = function(n, done) {
+  if (n === 0)
+    done(undefined, 0);
+  else if (n === 1 || n === 2)
+      done(undefined, 1);
+  else {
+    setImmediate(function() {
+      fibonacci(n-1, function(err, val1) {
+        if (err) done(err);
+        else setImmediate(function() {
+          fibonacci(n-2, function(err, val2) {
+            if (err) done(err);
+            else done(undefined, val1+val2);
+          });
+        });
+      });
+    });
   }
-  return fibos[n];
 }
