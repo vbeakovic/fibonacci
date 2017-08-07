2ba31ac10f85aafc00aee50c3ac8ed33ba4ff0b9
vbeakovic <valter.beakovic@gmail.com>
2017-08-07 11:22:51 +0200

    code refactored to use REST

--- routes/fibonacci.js
+++ routes/fibonacci.js
@@ -1,20 +1,29 @@
 const express = require('express');
 var router = express.Router();
 
-var math = require('../math');
-
-router.get('/', function(req, res,next) {
+router.get('/', function(req, res, next) {
   if (req.query.fibonum) {
-    math.fibonacci(req.query.fibonum, (err, fiboval) => {
-      res.render('fibonacci', {
-        title: "Calculate Fibonacci numbers",
-        fibonum: req.query.fibonum,
-        fiboval: fiboval
+    var httpreq = require('http').request({
+      host: "localhost",
+      port: process.enc.SERVERPORT,
+      path: "/fibonacci/"+Math.floor(req.query.fibonum),
+      method: 'GET'
+    }, httpresp => {
+      httpresp.on('data', chunk => {
+        var data = JSON.parse(chunk);
+        res.render('fibonacci', {
+          title: "Calculate Fibonacci numbers",
+          fibonum: req.query.fibonum,
+          fiboval: data.result
+        });
       });
+      httpresp.on('error', err => { next(err); });
     });
+    httpreq.on('error', err => { next(err); });
+    httpreq.end();
   } else {
     res.render('fibonacci', {
-      title: 'Calculate Fibonacci numbers',
+      title: "Calculate Fibonacci numbers",
       fiboval: undefined
     });
   }
